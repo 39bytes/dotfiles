67feaba81ca82e934342637e7b4b816c068f193b
39bytes <zhangjeff2014@gmail.com>
2024-06-24 13:09:59 +0000

    nvim: add more lsps to ensure installed

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -140,25 +140,13 @@ return { -- LSP Configuration & Plugins
     --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-    require('lspconfig').lua_ls.setup({})
-    require('lspconfig').tailwindcss.setup({})
-    require('lspconfig').tsserver.setup({})
-    require('lspconfig').rust_analyzer.setup({})
-    require('lspconfig').clangd.setup({})
+    require('lspconfig').lua_ls.setup {}
+    require('lspconfig').tailwindcss.setup {}
+    require('lspconfig').tsserver.setup {}
+    require('lspconfig').rust_analyzer.setup {}
+    require('lspconfig').clangd.setup {}
 
     local servers = {
-      -- clangd = {},
-      -- gopls = {},
-      -- pyright = {},
-      -- rust_analyzer = {},
-      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`tsserver`) will work just fine
-      -- tsserver = {},
-      --
       lua_ls = {
         -- cmd = {...},
         -- filetypes = { ...},
@@ -180,6 +168,10 @@ return { -- LSP Configuration & Plugins
           },
         },
       },
+      rust_analyzer = {},
+      tsserver = {},
+      tailwindcss = {},
+      clangd = {},
     }
 
     -- Ensure the servers and tools above are installed
@@ -188,30 +180,34 @@ return { -- LSP Configuration & Plugins
     --    :Mason
     --
     --  You can press `g?` for help in this menu.
-    -- require('mason').setup()
+    require('mason').setup()
 
     -- You can add other tools here that you want Mason to install
     -- for you, so that they are available from within Neovim.
-    -- local ensure_installed = vim.tbl_keys(servers or {})
-    -- vim.list_extend(ensure_installed, {
-    --   'stylua', -- Used to format Lua code
-    -- })
-    -- require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+    local ensure_installed = vim.tbl_keys(servers or {})
+    vim.list_extend(ensure_installed, {
+      'stylua', -- Used to format Lua code
+      'prettierd',
+      'clang-format',
+      'eslint-lsp',
+      'ruff-lsp',
+    })
+    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
-    -- require('mason-lspconfig').setup {
-    --   handlers = {
-    --     function(server_name)
-    --       local server = servers[server_name] or {}
-    --       -- This handles overriding only values explicitly passed
-    --       -- by the server configuration above. Useful when disabling
-    --       -- certain features of an LSP (for example, turning off formatting for tsserver)
-    --       server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-    --
-    --       require('lspconfig')[server_name].setup(server)
-    --     end,
-    --     ['tsserver'] = function() end,
-    --     ['rust_analyzer'] = function() end,
-    --   },
-    -- }
+    require('mason-lspconfig').setup {
+      handlers = {
+        function(server_name)
+          local server = servers[server_name] or {}
+          -- This handles overriding only values explicitly passed
+          -- by the server configuration above. Useful when disabling
+          -- certain features of an LSP (for example, turning off formatting for tsserver)
+          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+
+          require('lspconfig')[server_name].setup(server)
+        end,
+        ['tsserver'] = function() end,
+        ['rust_analyzer'] = function() end,
+      },
+    }
   end,
 }

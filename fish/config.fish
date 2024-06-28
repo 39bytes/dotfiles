851b21fff9a1af5a5a369f628eb64a1d58fbef3f
39bytes <zhangjeff2014@gmail.com>
2024-06-28 01:07:09 +0000

    fish: add pipx

--- fish/config.fish
+++ fish/config.fish
@@ -10,3 +10,6 @@ zoxide init fish | source
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+# Created by `pipx` on 2024-06-28 01:05:45
+set PATH $PATH /home/jeff/.local/bin

eb86e6158084b3adfa91191d4ca675f412105f4b
39bytes <zhangjeff2014@gmail.com>
2024-12-15 23:24:36 -0500

    nvim: try out fzf-lua

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,6 +2,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
   'nvim-telescope/telescope.nvim',
   event = 'VimEnter',
   branch = '0.1.x',
+  enabled = false,
   dependencies = {
     'nvim-lua/plenary.nvim',
     { -- If encountering errors, see telescope-fzf-native README for installation instructions

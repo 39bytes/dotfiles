0dbdde84e74df5b5cda92e7aff6ae48655410f64
39bytes <jeff@39bytes.dev>
2025-01-28 13:06:10 -0500

    nvim: use live_grep_glob for fzf-lua

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -19,7 +19,7 @@ return {
     vim.keymap.set('n', '<leader><leader>', fzf.files, { desc = 'Find files' })
     vim.keymap.set('n', '<leader>sr', fzf.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>sb', fzf.buffers, { desc = '[S]earch [B]uffers' })
-    vim.keymap.set('n', '<leader>sg', fzf.live_grep, { desc = '[S]earch by [G]rep' })
+    vim.keymap.set('n', '<leader>sg', fzf.live_grep_glob, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sq', fzf.quickfix, { desc = '[S]earch [Q]uickfix' })
     vim.keymap.set('n', '<leader>bl', fzf.lines, { desc = 'Search [B]uffer [L]ines' })
     vim.keymap.set('n', '<leader>s.', fzf.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })

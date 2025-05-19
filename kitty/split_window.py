3586f4f8ec6fc20b3440ae6bdd1599ddb67ce4e2
39bytes <jeff@39bytes.dev>
2025-05-19 11:34:24 -0700

    kitty: smart-splits

--- kitty/split_window.py
+++ kitty/split_window.py
@@ -1,15 +1,9 @@
-import re
-
 from kittens.tui.handler import result_handler
 from kitty.key_encoding import KeyEvent, parse_shortcut
 
 
-def is_window_vim(window, vim_id):
-    fp = window.child.foreground_processes
-    return any(
-        re.search(vim_id, p["cmdline"][0] if len(p["cmdline"]) else "", re.I)
-        for p in fp
-    )
+def main(args):
+    pass
 
 
 def encode_key_mapping(window, key_mapping):
@@ -28,23 +22,28 @@ def encode_key_mapping(window, key_mapping):
     return window.encoded_key(event)
 
 
-def main():
-    pass
+def split_window(boss, direction):
+    if direction == 'up' or direction == 'down':
+        boss.launch('--cwd=current', '--location=hsplit')
+    else:
+        boss.launch('--cwd=current', '--location=vsplit')
+
+    if direction == 'up' or direction == 'left':
+        boss.active_tab.move_window(direction)
 
 
 @result_handler(no_ui=True)
 def handle_result(args, result, target_window_id, boss):
-    direction = args[1]
-    key_mapping = args[2]
-    vim_id = args[3] if len(args) > 3 else "n?vim"
-
     window = boss.window_id_map.get(target_window_id)
 
     if window is None:
         return
-    if is_window_vim(window, vim_id):
-        for keymap in key_mapping.split(">"):
-            encoded = encode_key_mapping(window, keymap)
-            window.write_to_child(encoded)
+
+    direction = args[1]
+    cmd = window.child.foreground_cmdline[0]
+    if cmd == 'tmux':
+        keymap = args[2]
+        encoded = encode_key_mapping(window, keymap)
+        window.write_to_child(encoded)
     else:
-        boss.active_tab.neighboring_window(direction)
+        split_window(boss, direction)
